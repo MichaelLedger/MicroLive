ccc4a60fa0656819962a1908d96c1ccbf3a38030
Mountain Xiang <783600983@qq.com>
2018-11-14 15:26:09 +0800

    初始化项目&CocoaPods

---
+++ MicroLive/MicroLive/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MicroLive
+//
+//  Created by MountainX on 2018/11/14.
+//  Copyright © 2018年 MTX Software Technology Co.,Ltd. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
